1d1dc62cbb3606c737758cdd77362f93f8570c34
Bruno Silveira <dasilveira.bru@terra.com.br>
2025-05-31 18:58:07 -0300

    Mega tester pronto

--- etapa5/testes/E5_TESTER/tester.sh
+++ etapa5/testes/E5_TESTER/tester.sh
@@ -3,15 +3,21 @@ COMPILER_PATH=$1
 TESTES=(
     "dec_atrib"
     "aritmetica"
+    "logicas"
+    "if"
+    "if_else"
+    "while"
+    "if_vazio"
 )
-TESTE="logicas"
-$COMPILER_PATH < ./$TESTE/codigo.txt > $TESTE/iloc.txt
-while [ $? != 0 ]; do
-    $COMPILER_PATH < ./$TESTE/codigo.txt > ./$TESTE/iloc.txt
+for TESTE in "${TESTES[@]}"; do
+    $COMPILER_PATH < ./$TESTE/codigo.txt > $TESTE/iloc.txt 2>/dev/null
+    while [ $? != 0 ]; do
+        $COMPILER_PATH < ./$TESTE/codigo.txt > $TESTE/iloc.txt 2>/dev/null
+    done
+    python3 ./ilocsim.py -m $TESTE/iloc.txt > $TESTE/resultado_sim.txt
+    if cmp -s ./$TESTE/resultado_sim.txt ./$TESTE/gabarito.txt; then
+        echo "Teste $TESTE passou"
+    else
+        echo "Teste $TESTE falhou"
+    fi
 done
-python3 ./ilocsim.py -m $TESTE/iloc.txt > $TESTE/resultado_sim.txt
-if cmp -s ./$TESTE/resultado_sim.txt ./$TESTE/gabarito.txt; then
-    echo "Teste $TESTE passou"
-else
-    echo "Teste $TESTE falhou"
-fi
